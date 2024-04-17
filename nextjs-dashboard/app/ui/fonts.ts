d2b72564193718f90386e77bf36deea203fca6a9
“Mkiza” <ronaldbagenda@gmail.com>
2024-04-17 16:51:44 +0200

    tutorial

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter } from 'next/font/google';
+// Import Lusitana font from Google Fonts
+import { Lusitana } from 'next/font/google';
+ 
+
+
+export const inter = Inter({ subsets: ['latin'] });
+// export the lusitana font 
+export const lusitana = Lusitana({ weight: '400', subsets: ['latin'] });
